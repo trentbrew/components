f75354136ce33f8538c1840fd8f0637d0db5d85a
Trent Brew <tbrew1023@gmail.com>
2022-02-22 18:37:50 -0600

    clean up App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,3 @@
 <template>
-  <!-- 
-  
-  Vue 3 Input App
-  Instructions:
-
-  1. Create Fork Version of this repo [x]
-  2. Create default "/" route to a Create.vue /pages [x]
-  3. Recreate the following input field as seen in image [x]
-  4. Create save state, update state, and cancel puts [x]
-  data back to last saved state buttons
-  4. Turn this input into a /components and display on
-  Create.vue on setup [x]
-  
-  Dependencies:
-  The following dependencies are already loaded.
-  Please use the following:
-  1. Pinia https://pinia.vuejs.org/ (required) [x]
-  2. Vue Router https://router.vuejs.org/ (required) [x]
-  3. TailwindCSS https://tailwindcss.com/docs/ (required) [x]
-  5. VueUse https://vueuse.org/ (optional) [x]
-
-  UI:
-  1. SVG Icons https://www.xicons.org/#/ [x]
-
-  You are welcome to add other dependancies
-  please list what has been added below:
-  1.
-  
-  -->
-
   <router-view />
-
 </template>
